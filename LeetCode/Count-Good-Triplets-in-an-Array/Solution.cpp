fcb0972b93b23501562bca9eb5d112862f5aa58d
Shubhang Gupta <guptashubhang2000@gmail.com>
2025-04-15 22:10:38 +0530

    Add solution for Count Good Triplets in an Array

---
+++ LeetCode/Count-Good-Triplets-in-an-Array/Solution.cpp
@@ -0,0 +1,5 @@
+Input: nums1 = [2,0,1,3], nums2 = [0,1,2,3]
+Output: 1
+Explanation: 
+There are 4 triplets (x,y,z) such that pos1x < pos1y < pos1z. They are (2,0,1), (2,0,3), (2,1,3), and (0,1,3). 
+Out of those triplets, only the triplet (0,1,3) satisfies pos2x < pos2y < pos2z. Hence, there is only 1 good triplet.
