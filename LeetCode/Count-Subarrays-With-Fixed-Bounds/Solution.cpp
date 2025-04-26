69e0e57036967230b1b493ac789c73b62416ad02
Shubhang Gupta <guptashubhang2000@gmail.com>
2025-04-26 23:09:05 +0530

    Add solution for Count Subarrays With Fixed Bounds

---
+++ LeetCode/Count-Subarrays-With-Fixed-Bounds/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long long ans = 0;
+        int j1 = -1, j2 = -1, k = -1;
+        for (int i = 0; i < static_cast<int>(nums.size()); ++i) {
+            if (nums[i] < minK || nums[i] > maxK) {
+                k = i;
+            }
+            if (nums[i] == minK) {
+                j1 = i;
+            }
+            if (nums[i] == maxK) {
+                j2 = i;
+            }
+            ans += max(0, min(j1, j2) - k);
+        }
+        return ans;
+    }
+};
