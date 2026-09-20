5e6f9becc7dcf16d03a7916973e718716957d4a0
Shubhang Gupta <guptashubhang2000@gmail.com>
2026-09-20 13:53:58 +0530

    Add solution for Reverse Degree of a String

--- LeetCode/Reverse-Degree-of-a-String/Solution.cpp
+++ LeetCode/Reverse-Degree-of-a-String/Solution.cpp
@@ -1,2 +1,12 @@
-    }
-};
+1class Solution {
+2public:
+3    int reverseDegree(string s) {
+4        int n = s.length();
+5        int ans = 0;
+6        for (int i = 1; i <= n; ++i) {
+7            int x = 26 - (s[i - 1] - 'a');
+8            ans += i * x;
+9        }
+10        return ans;
+11    }
+12};
